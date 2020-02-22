63fcbaa32345ed058128a11ea4e30cead3610bc1
Alexander <ailukyanov90@gmail.com>
2020-02-23 01:21:34 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var hamburger = document.getElementById("hamburger"),
+        aside = document.querySelector(".aside");
+
+    hamburger.addEventListener("click", function () {
+        this.classList.toggle("is-active");
+        aside.classList.toggle("shown");
+    });
+
+
+});
